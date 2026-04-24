657d07c26f5c3537ce75d56c8a9ebac5d83c69c1
axioe <yhkimb@naver.com>
2026-04-24 11:31:20 +0900

    #2 Interface 생성

--- src/crudInterface/CrudInterface.java
+++ src/crudInterface/CrudInterface.java
@@ -4,4 +4,16 @@ import javax.xml.stream.events.Comment;
 import java.util.List;
 public interface CrudInterface {
 
+    // 게시글
+    List<Article> all();
+    void    newArticle(Article article);
+    Article detail(Long id);
+    boolean delete(Long id);
+    void    update(Article article);
+
+    // 댓글
+    void insertComment(Comment comment);
+    void updateComment(Comment comment);
+    void deleteComment(Long deleteCommentId);
+    
 }

74f92e22c7f4c4458ab63224e999925184636d92
axioe <yhkimb@naver.com>
2026-04-24 12:47:39 +0900

    #3 Repository 수정 완료

--- src/repository/ArticleRepository.java
+++ src/repository/ArticleRepository.java
@@ -4,48 +4,78 @@ import crudInterface.CrudInterface;
 import dto.ArticleDto;
 import dto.CommentDto;
 
-import javax.xml.stream.events.Comment;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ArticleRepository implements CrudInterface {
 
+    private static Long ariticlId = 1L;
+    private static Long commentId = 1L;
+    private static List<ArticleDto> articleList = new ArrayList<>();
+
+
+
     @Override
     public List<ArticleDto> all() {
-        return List.of();
+        return articleList;
     }
 
     @Override
     public void newArticle(ArticleDto articleDto) {
-
+        articleDto.setId(ariticlId++);
+        articleList.add(articleDto);
     }
 
     @Override
     public ArticleDto detail(Long id) {
+        for (ArticleDto article : articleList) {
+            if (article.getId().equals(id)) {
+                return article;
+            }
+        }
         return null;
     }
 
     @Override
     public boolean delete(Long id) {
-        return false;
+        return articleList.removeIf(article -> article.getId().equals(id));
     }
 
     @Override
     public void update(ArticleDto articleDto) {
-
+        for (int i = 0; i < articleList.size(); i++) {
+            if (articleList.get(i).getId().equals(articleDto.getId())) {
+                articleList.set(i, articleDto);
+                return;
+            }
+        }
     }
 
     @Override
     public void insertComment(CommentDto commentDto) {
-
+        ArticleDto article = detail(commentDto.getArticleId());
+        if (article != null) {
+            commentDto.setCommentId(commentId++);
+            article.addComments(commentDto);
+        }
     }
 
     @Override
     public void updateComment(CommentDto commentDto) {
-
+        for (ArticleDto article : articleList) {
+            for (CommentDto comment : article.getCommentDtoList()) {
+                if (comment.getCommentId().equals(commentDto.getCommentId())) {
+                    comment.setContent(commentDto.getContent());
+                    return;
+                }
+            }
+        }
     }
 
     @Override
     public void deleteComment(Long deleteCommentId) {
-
+        for (ArticleDto article : articleList) {
+            article.getCommentDtoList().removeIf(c -> c.getCommentId().equals(deleteCommentId));
+        }
     }
 }

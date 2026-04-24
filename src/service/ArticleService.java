229c43e1acc26988e9920a35f978174424828935
axioe <yhkimb@naver.com>
2026-04-24 15:54:11 +0900

    #6 서비스 부분 업데이트

--- src/service/ArticleService.java
+++ src/service/ArticleService.java
@@ -1,4 +1,30 @@
 package service;
 
+import crudInterface.CrudInterface;
+import dao.ArticleDAO;
+import dto.ArticleDto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class ArticleService {
+    private CrudInterface repository = new ArticleDAO();
+
+    public List<ArticleDto> all(){
+        return repository.all();
+    }
+    public  void newArticle(ArticleDto dto){
+        dto.setInsertedDate(LocalDateTime.now());
+        repository.newArticle(dto);
+    }
+    public ArticleDto detail(Long id){
+        return repository.detail(id);
+    }
+    public boolean delete(long id){
+        return repository.delete(id);
+    }
+    public void update(ArticleDto dto){
+        dto.setUpdatedDate(LocalDateTime.now());
+        repository.update(dto);
+    }
 }
--- src/service/CommentService.java
+++ src/service/CommentService.java
@@ -1,4 +1,24 @@
 package service;
 
+import crudInterface.CrudInterface;
+import dao.ArticleDAO;
+import dto.ArticleDto;
+import dto.CommentDto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class CommentService {
+    private  CrudInterface dao = new ArticleDAO();
+
+    public void commentAdd(CommentDto comment){
+        dao.insertComment(comment);
+    }
+    public  void  commentUpdate(CommentDto comment){
+        dao.updateComment(comment);
+    }
+    public void commentDelete(Long deleteCommentId){
+        dao.deleteComment(deleteCommentId);
+    }
+
 }
